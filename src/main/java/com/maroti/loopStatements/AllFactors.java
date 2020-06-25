e6a96d6052e3647cd9a51166cc1495c9cb72e64b
Arnav <pawarmw@gmail.com>
2020-06-25 06:27:09 +0530

    Solved! All Factors. Closes #22

---
+++ src/main/java/com/maroti/loopStatements/AllFactors.java
@@ -0,0 +1,25 @@
+package com.maroti.loopStatements;
+
+public class AllFactors {
+
+    public static void printFactors(int number)
+    {
+        if(number<1)
+        {
+            System.out.println("invalid Number");
+        }else{
+
+            for(int i=1; i<=number; i++)
+            {
+                if(number%i==0)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFactors(10);
+    }
+}
